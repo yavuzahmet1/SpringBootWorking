a4aae4571de95be5402416f35eda391c79cab860
Coders club <1yavuzahmet@gmail.com>
2022-09-14 00:06:19 +0300

    spring start files created

---
+++ PatikaSpringFramework/src/main/java/com/ahmetyavuz/bean/JsfBean.java
@@ -0,0 +1,20 @@
+package com.ahmetyavuz.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value ="cdiBean")
+@ApplicationScoped
+@Setter
+@Getter
+public class JsfBean {
+    private String patika;
+
+    public JsfBean(){
+        patika="Spring Egitimine Hos geldiniz...";
+    }
+
+}
