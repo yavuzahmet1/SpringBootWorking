e5b0988c9ad59d286863c52c85119447e3a13117
Coders club <1yavuzahmet@gmail.com>
2022-11-04 22:29:08 +0300

    department-service new created on maven

---
+++ deparment-service/deparment-service/src/main/java/com/dailycodebuffer/deparmentservice/DeparmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.deparmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeparmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeparmentServiceApplication.class, args);
+	}
+
+}
---
+++ deparment-service/deparment-service/src/test/java/com/dailycodebuffer/deparmentservice/DeparmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.deparmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeparmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
