0cbed2456c0ddea0965e06a8aaa4eab9b215b6b1
Coders club <1yavuzahmet@gmail.com>
2022-11-29 13:19:54 +0300

    library-dev created from folksdev

---
+++ library-dev/Library-dev/src/main/java/com/backend/library/LibraryDevApplication.java
@@ -0,0 +1,13 @@
+package com.backend.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDevApplication.class, args);
+	}
+
+}
---
+++ library-dev/Library-dev/src/test/java/com/backend/library/LibraryDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
