3bd26771039cda4fb3a9fd3626b2ea0d96ffbe01
Coders club <1yavuzahmet@gmail.com>
2022-12-02 14:54:05 +0300

    spring-boot-backend prj created

---
+++ springboot-backend-prj/springboot-backend-prj/src/main/java/com/javabackend/springbootbackendprj/model/Employee.java
@@ -0,0 +1,24 @@
+package com.javabackend.springbootbackendprj.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_id")
+    private String emailId;
+}
