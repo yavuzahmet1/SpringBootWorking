ecd67387d62a7d522a004918059f4b4b7b7f777b
Coders club <1yavuzahmet@gmail.com>
2022-12-03 14:23:11 +0300

    react component crreated

--- springboot-backend-prj/springboot-backend-prj/src/main/java/com/javabackend/springbootbackendprj/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javabackend.springbootbackendprj.repository;
-
-import com.javabackend.springbootbackendprj.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
