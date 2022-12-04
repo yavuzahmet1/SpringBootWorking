1d4218baec36f46b389346b6c9a4cc64f04cdec5
Coders club <1yavuzahmet@gmail.com>
2022-12-04 12:00:07 +0300

    e-commerance-shopping created project and layers

---
+++ e-commerence-shopping/e-commerence-shopping/src/main/java/com/javaeducation/commerence/SpringbootBackendPrjApplication.java
@@ -0,0 +1,13 @@
+package com.javaeducation.commerence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendPrjApplication.class, args);
+	}
+
+}
---
+++ e-commerence-shopping/e-commerence-shopping/src/test/java/com/javaeducation/commerence/SpringbootBackendPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeducation.commerence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
