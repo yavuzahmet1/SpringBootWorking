6e3b86dc9e0faf9e1a44cf375dec054711d18b1e
Coders club <1yavuzahmet@gmail.com>
2023-02-20 13:50:01 +0300

    celendar controller created CRUD

--- content-calendar/src/main/java/dev/danvega/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/dev/danvega/contentcalendar/controller/ContentController.java
@@ -1,10 +1,46 @@
 package dev.danvega.contentcalendar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.danvega.contentcalendar.model.Content;
+import dev.danvega.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found this ID"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, Integer id) {
+        if (!repository.existById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+        repository.save(content);
+    }
 
 }
--- content-calendar/src/main/java/dev/danvega/contentcalendar/repository/ContentCollectionRepository.java
+++ content-calendar/src/main/java/dev/danvega/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,13 +1,56 @@
 package dev.danvega.contentcalendar.repository;
 
 import dev.danvega.contentcalendar.model.Content;
+import dev.danvega.contentcalendar.model.Status;
+import dev.danvega.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
     private final List<Content> contents = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+    }
+
+    public List<Content> findAll() {
+        return contents;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return contents.stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(1,
+                "My first Blog Post",
+                "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+        contents.add(c);
+    }
+
+    public void save(Content content) {
+        contents.removeIf(c->c.id().equals(content.id()));
+        contents.add(content);
+    }
+
+    public boolean existById(Integer id) {
+        return contents.stream()
+                .filter(c -> c.id().equals(id))
+                .count() == 1;
+    }
+
+    ///1.30 da kaldım
 }
-///Spring Boot tutorials dakika 1.00'de kaldım
