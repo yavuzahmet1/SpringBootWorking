9ce728020cfdd711dce2031f9e7192db05ca622f
Coders club <1yavuzahmet@gmail.com>
2023-02-25 13:54:25 +0300

    week 1 js worked

---
+++ java-script-master/src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba kodlama.io")
+
+let dolarDun=9.20
+dolarDun="Ankara"
+let dolarBugun=9.30
+{
+    let dolarDun=9.05
+}
+
+console.log(dolarDun)
+
+const euroDun=11.22
+//euroDun=11
+
+console.log(euroDun)
+
+
+//array
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Deprem Kredisi"]
+//console.log(konutKredileri)
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("konut kredileri "+ konutKredileri[index])
+    
+}
