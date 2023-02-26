6601183f752911c1c187fc7700dfe235b7c8a2e7
Coders club <1yavuzahmet@gmail.com>
2023-02-27 00:12:56 +0300

    week 1 js worked prototyping

---
+++ java-script-master/src/oop/index.js
@@ -0,0 +1,34 @@
+class Customer{
+    constructor(id,customerNumber){
+        this.id=id
+        this.customerNumber=customerNumber
+    }
+}
+
+let customer=new Customer(1,"123555");
+
+//prototyping
+customer.name="Ahmet YAVUZ"
+console.log(customer.name)
+Customer.bisey="Bisey"
+
+console.log(Customer.bisey)//Bisey->instance'ye yapılan prototyping
+
+console.log(customer.bisey)//undefined->class'a yapılan prototyping
+
+console.log(customer.customerNumber)
+console.log(customer.id)
+
+class IndividualCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+    super(id,customerNumber)
+    this.firstName=firstName
+    }
+}
+
+class CorporateCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+    super(id,customerNumber)
+    this.companyName=companyName
+    }
+}
