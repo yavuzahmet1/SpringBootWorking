77f96e0d5990a6f03137ac9978891c84ec9da71b
Coders club <1yavuzahmet@gmail.com>
2023-03-14 12:16:15 +0300

    spring-backend file created

---
+++ course-spring-boot-react/wshoxi/wshoxi/src/test/java/com/hoxi/WshoxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshoxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
