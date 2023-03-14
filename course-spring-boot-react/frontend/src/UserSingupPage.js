30ac9cea0e0080bcca43192db90fa6b0f50912f3
Coders club <1yavuzahmet@gmail.com>
2023-03-14 11:47:06 +0300

    course-spring-boot-react project singup object destruc event

--- course-spring-boot-react/frontend/src/UserSingupPage.js
+++ course-spring-boot-react/frontend/src/UserSingupPage.js
@@ -3,22 +3,49 @@ import React from "react";
 class UserSingupPage extends React.Component {
     state = {
         username: null,
-        displayname:null
-
-
+        displayname: null,
+        password: null,
+        passwordRepeat: null
     };
 
+    onChange=event=>{
+
+        const{name,value}=event.target; //object destructure anlamına gelir kullnımı best prectisedir
+        this.setState({
+            [name]:value
+        });
+
+        /*const value=event.target.value;
+        const field=event.target.name;
+        this.setState({
+            [field]:value
+        });*/
+    }
+/*
     onChangeUserName = event => {
         this.setState({
             username: event.target.value
-        })
+        });
     };
+
     onChangeDisplayName = event => {
         this.setState({
             displayname: event.target.value
-        })
+        });
     };
 
+    onChangePassword = event => {
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    onChangePasswordRepeat = event => {
+        this.setState({
+            passwordRepeat: event.target.value
+        });
+    };
+*/
 
     render() {
         return (
@@ -26,19 +53,19 @@ class UserSingupPage extends React.Component {
                 <h1>Sign Up</h1>
                 <div>
                     <label>UserName</label>
-                    <input onChange={this.onChangeUserName} />
+                    <input name="username" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>DisplayName</label>
-                    <input onChange={this.onChangeDisplayName} />
+                    <input name="displayname" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type={"password"} />
+                    <input name="password" type={"password"} onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password" />
+                    <input name="passwordRepeat" type={"password"} onChange={this.onChange} />
                 </div>
                 <button>Sing Up</button>
             </form>
