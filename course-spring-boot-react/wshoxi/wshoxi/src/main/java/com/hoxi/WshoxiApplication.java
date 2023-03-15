9fda2f72ef7c6084623992c670b00f059ec1a721
Coders club <1yavuzahmet@gmail.com>
2023-03-15 12:57:58 +0300

    frontend-backend securty depencenies added

--- course-spring-boot-react/wshoxi/wshoxi/src/main/java/com/hoxi/WshoxiApplication.java
+++ course-spring-boot-react/wshoxi/wshoxi/src/main/java/com/hoxi/WshoxiApplication.java
@@ -2,8 +2,12 @@ package com.hoxi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class )
+//Spring Security pom.xml'de yüklü olduğunbdan güvenlik gerekçesiyle password isteyecektir.
+//işte bunu geçici olarak kaldırabiliriz bunun için (exclude=SecurityAutoConfiguration.class) yazılır ve
+//geçici oarak bunu devredışı brakabiliriz.
 public class WshoxiApplication {
 
 	public static void main(String[] args) {
--- course-spring-boot-react/wshoxi/wshoxi/src/main/java/com/hoxi/service/UserService.java
+++ course-spring-boot-react/wshoxi/wshoxi/src/main/java/com/hoxi/service/UserService.java
@@ -2,18 +2,26 @@ package com.hoxi.service;
 
 import com.hoxi.entity.User;
 import com.hoxi.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;//passworları encode eder
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void saveUser(User user) {
+        /*String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);*/ //veya aşağıdaki gibi kısacada yazılabilir.
+
+        user.setPassword(this.passwordEncoder.encode(user.getPassword())); //password encode oldu
 
         userRepository.save(user);
 
